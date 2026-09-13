cab15c1a6cc351edc95ceef53ee27cd6a002a63e
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 03:29:04 +0530

    Update 3-AppointmentService.java

--- 1-src/3-service/3-AppointmentService.java
+++ 1-src/3-service/3-AppointmentService.java
@@ -81,4 +81,28 @@ public class AppointmentService {
         return appointmentDAO.getAppointmentById(appointmentId);
     }
 
-  
+    // Cancel appointment
+    public boolean cancelAppointment(int appointmentId) {
+
+        if (appointmentId <= 0) {
+            System.out.println("Invalid appointment ID.");
+            return false;
+        }
+
+        Appointment appointment =
+                appointmentDAO.getAppointmentById(appointmentId);
+
+        if (appointment == null) {
+            System.out.println("Appointment not found.");
+            return false;
+        }
+
+        if (appointment.getStatus().equalsIgnoreCase("Cancelled")) {
+            System.out.println("Appointment is already cancelled.");
+            return false;
+        }
+
+        return appointmentDAO.cancelAppointment(appointmentId);
+    }
+}
+

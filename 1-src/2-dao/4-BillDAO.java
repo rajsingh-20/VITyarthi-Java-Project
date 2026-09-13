b43038aeeb0df53622e9278f990da256527aa6ef
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 03:17:03 +0530

    Update 4-BillDAO.java

--- 1-src/2-dao/4-BillDAO.java
+++ 1-src/2-dao/4-BillDAO.java
@@ -1,26 +1,2 @@
-// CANCEL APPOINTMENT
-    public boolean cancelAppointment(int appointmentId) {
 
-        String sql =
-                "UPDATE appointments " +
-                "SET status='Cancelled' " +
-                "WHERE appointment_id=?";
-
-        try (Connection con = DatabaseConnection.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, appointmentId);
-
-            return ps.executeUpdate() > 0;
-
-        } catch (SQLException e) {
-            System.out.println(
-                    "Error cancelling appointment."
-            );
-            e.printStackTrace();
-            return false;
-        }
-    }
-}
-```
 

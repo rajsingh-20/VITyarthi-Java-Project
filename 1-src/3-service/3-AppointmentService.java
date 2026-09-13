11c0da1fe44e050d0e2119afb69455b42c3387ce
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 03:27:18 +0530

    Remove deleteDoctor method from AppointmentService

--- 1-src/3-service/3-AppointmentService.java
+++ 1-src/3-service/3-AppointmentService.java
@@ -1,11 +1,1 @@
-  // Delete doctor
-    public boolean deleteDoctor(int doctorId) {
-
-        if (doctorId <= 0) {
-            System.out.println("Invalid doctor ID.");
-            return false;
-        }
-
-        return doctorDAO.deleteDoctor(doctorId);
-    }
-}
+ 

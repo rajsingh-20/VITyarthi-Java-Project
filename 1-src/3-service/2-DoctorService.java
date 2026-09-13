51aa06ead33a83c1aa194cc90c43ce4fb118d9ee
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 03:27:08 +0530

    Add deleteDoctor method to DoctorService

--- 1-src/3-service/2-DoctorService.java
+++ 1-src/3-service/2-DoctorService.java
@@ -76,4 +76,14 @@ public class DoctorService {
         return doctorDAO.updateDoctor(doctor);
     }
 
-  
+    // Delete doctor
+    public boolean deleteDoctor(int doctorId) {
+
+        if (doctorId <= 0) {
+            System.out.println("Invalid doctor ID.");
+            return false;
+        }
+
+        return doctorDAO.deleteDoctor(doctorId);
+    }
+}

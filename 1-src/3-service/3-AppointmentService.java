16dfeba6d78fda461b709e66892349f50d642875
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 03:26:50 +0530

    Update 3-AppointmentService.java

--- 1-src/3-service/3-AppointmentService.java
+++ 1-src/3-service/3-AppointmentService.java
@@ -1,1 +1,11 @@
+  // Delete doctor
+    public boolean deleteDoctor(int doctorId) {
 
+        if (doctorId <= 0) {
+            System.out.println("Invalid doctor ID.");
+            return false;
+        }
+
+        return doctorDAO.deleteDoctor(doctorId);
+    }
+}

93f828f235419e715604cb928c1479bdc6c4335a
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 02:57:13 +0530

    Update 2-Doctor.java

--- 1-src/1-model/2-Doctor.java
+++ 1-src/1-model/2-Doctor.java
@@ -1,1 +1,62 @@
+package model;
 
+public class Doctor {
+
+    private int doctorId;
+    private String name;
+    private String specialization;
+    private String phone;
+    private int experience;
+
+    public Doctor() {
+    }
+
+    public Doctor(String name, String specialization,
+                   String phone, int experience) {
+
+        this.name = name;
+        this.specialization = specialization;
+        this.phone = phone;
+        this.experience = experience;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}

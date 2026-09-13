454a6609de61e8691e06da8df090a02b7e78332e
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 02:56:52 +0530

    Add Patient class with attributes and methods

--- 1-src/1-model/1-Patient.java
+++ 1-src/1-model/1-Patient.java
@@ -1,1 +1,82 @@
+package model;
 
+public class Patient {
+
+    private int patientId;
+    private String name;
+    private int age;
+    private String gender;
+    private String phone;
+    private String address;
+    private String disease;
+
+    public Patient() {
+    }
+
+    public Patient(String name, int age, String gender,
+                   String phone, String address, String disease) {
+
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.disease = disease;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+}

04584c97933fb840b3681675b16e6fa1c7e1b282
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 04:06:49 +0530

    Remove database creation commands from 4-Bill.sql
    Removed database creation and usage commands from the SQL file.

--- 2-database/4-Bill.sql
+++ 2-database/4-Bill.sql
@@ -1,15 +1,4 @@
 -- ============================================
--- HOSPITAL MANAGEMENT SYSTEM DATABASE
--- ============================================
-
--- Create Database
-CREATE DATABASE hospital_db;
-
--- Use Database
-USE hospital_db;
-
-
--- ============================================
 -- BILLS TABLE
 -- ============================================
 

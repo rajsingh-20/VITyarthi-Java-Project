53ffc13b5eb6052b22f096811af19c1d7c16f5a8
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 05:56:13 +0530

    Update Database.sql

--- database/Database.sql
+++ database/Database.sql
@@ -1,1 +1,9 @@
+-- ============================================
+-- HOSPITAL MANAGEMENT SYSTEM DATABASE
+-- ============================================
 
+-- Create Database
+CREATE DATABASE hospital_db;
+
+-- Use Database
+USE hospital_db;

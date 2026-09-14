8f9d58e0831bc3a40460c167845d1b90896de271
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 06:45:54 +0530

    Add SQL imports to PatientDAO
    Added necessary imports for database operations.

--- src/dao/PatientDAO.java
+++ src/dao/PatientDAO.java
@@ -6,6 +6,10 @@ import util.DatabaseConnection;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PatientDAO {
 

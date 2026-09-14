c38258b070177ab8f2892c910a7f917713f5c805
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 06:43:40 +0530

    Update AppointmentDAO.java

--- src/dao/AppointmentDAO.java
+++ src/dao/AppointmentDAO.java
@@ -7,6 +7,10 @@ import util.DatabaseConnection;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AppointmentDAO {
 

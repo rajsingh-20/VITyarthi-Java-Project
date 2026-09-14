e1870950d2d2040ae59e633336109e61bedd5d63
Raj Singh <raj.25bai10997@vitbhopal.ac.in>
2026-09-14 05:32:22 +0530

    Delete database.sql directory

--- database.sql/Database.sql
+++
@@ -1,9 +0,0 @@
--- ============================================
--- HOSPITAL MANAGEMENT SYSTEM DATABASE
--- ============================================
-
--- Create Database
-CREATE DATABASE hospital_db;
-
--- Use Database
-USE hospital_db;
